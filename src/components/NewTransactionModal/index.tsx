25296d740e959a815fd24fc8573474fcce01cea0
Clara Brancalhão <brancalhaoclara@gmail.com>
2022-04-29 20:27:53 -0300

    feat: create new transaction modal content

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,39 @@
+import Modal from "react-modal";
+import { Container } from "./styles";
+import closeModalImg from "../../assets/close.svg";
+
+Modal.setAppElement("#root");
+
+interface NewTransactionModalProps {
+  isOpen: boolean;
+  onRequestClose: () => void;
+}
+
+export const NewTransactionModal = ({
+  isOpen,
+  onRequestClose,
+}: NewTransactionModalProps) => {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeModalImg} alt="close-modal" />
+      </button>
+      <Container>
+        <h2>Cadastrar transação</h2>
+        <input type="text" placeholder="Título" />
+        <input type="number" placeholder="Valor" />
+        <input type="text" placeholder="Categoria" />
+        <button type="submit">Cadastrar</button>
+      </Container>
+    </Modal>
+  );
+};
